34e56afc6401649566a1141963a1bdec1cf53abb
edwinkepler <edwinkepler@protonmail.com>
2018-06-25 20:47:01 +0200

    Add main nav bar and logo

---
+++ includes/header.php
@@ -0,0 +1,56 @@
+<?php
+require 'config/config.php';
+
+if(isset($_SESSION["username"])) {
+    $userLoggedIn = $_SESSION["username"];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
+} else {
+    header("Location: register.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Swirlfeed</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="top-bar">
+        <div class="logo">
+            <a href="index.php">Swirlfeed!</a>
+        </div>
+        <nav>
+            <a href="#">
+                <?php
+                echo $user["first_name"];
+                ?>
+            </a>
+            <a href="index.php">
+                <i class="fa fa-home fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-envelope fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-bell fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-users fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-cog fa-lg"></i>
+            </a>
+        </nav>
+    </div>
