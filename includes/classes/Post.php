4efd1a91b86d9bef20d08e8b69f4eb66896df894
edwinkepler <edwinkepler@protonmail.com>
2018-07-03 21:29:18 +0200

    Add ajax loading posts

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -43,104 +43,134 @@ class Post
         }
     }
 
-    public function loadPostsFriends()
+    public function loadPostsFriends($data, $limit)
     {
-        $str = ""; // string to return
-        $data = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
-
-        while ($row = mysqli_fetch_array($data)) {
-            $id = $row["id"];
-            $body = $row["body"];
-            $added_by = $row["added_by"];
-            $date_time = $row["date_added"];
-
-            // Prepare user_to string so it can be included even if not posted to a user
-            if ($row["user_to"] == "none") {
-                $user_to = "";
-            } else {
-                $user_to_obj = new User($this->con, $row["user_to"]);
-                $user_to_name = $user_to_object->getFirstAndLastName();
-                $user_to = "to <a href='" . $row["user_to"] . "'>" . $user_to_name . "</a>";
-            }
-
-            // Check if user who posted, has their account closed
-            $added_by_obj = new User($this->con, $added_by);
-            if ($added_by_obj->isClosed()) {
-                continue;
-            }
-
-            $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
-            $user_row = mysqli_fetch_array($user_details_query);
-            $first_name = $user_row["first_name"];
-            $last_name = $user_row["last_name"];
-            $profile_pic = $user_row["profile_pic"];
-
-            // Timeframe
-            $date_time_now = date("Y-m-d H:i:s");
-            $start_date = new DateTime($date_time); // Time of post
-            $end_date = new DateTime($date_time_now); // Current time
-            $interval = $start_date->diff($end_date); // Difference between dates
-            if ($interval->y >= 1) {
-                if ($interval == 1) {
-                    $time_message = $interval->y . " year ago"; // 1 year ago
-                } else {
-                    $time_message = $interval->y . " years ago"; // More then 1 year ago
-                }
-            } elseif ($interval->m >= 1) {
-                if ($interval->d == 0) {
-                    $days = " ago";
-                } elseif ($interval->d == 1) {
-                    $days = $interval->d . " day ago";
-                } else {
-                    $days = $interval->d . " days ago";
-                }
-
-                if ($interval->m == 1) {
-                    $time_message = $interval->m . " month" . $days;
-                } else {
-                    $time_message = $interval->m . " months" . $days;
-                }
-            } elseif ($interval->d >= 1) {
-                if ($interval->d == 1) {
-                    $time_message = "Yesterday";
-                } else {
-                    $time_message = $interval->d . " days ago";
-                }
-            } elseif ($interval->h >= 1) {
-                if ($interval->h == 1) {
-                    $time_message = $interval->h . " hour ago";
-                } else {
-                    $time_message = $interval->h . " hours ago";
-                }
-            } elseif ($interval->i >= 1) {
-                if ($interval->i == 1) {
-                    $time_message = $interval->i . " minute ago";
-                } else {
-                    $time_message = $interval->i . " minutes ago";
-                }
-            } else {
-                if ($interval->s < 30) {
-                    $time_message = "Just now";
-                } else {
-                    $time_message = $interval->s . " seconds ago";
-                }
-            }
-
-            $str .= "<div class='status-post'>
-                        <div class='post-profile-pic'>
-                            <img src='$profile_pic' width='50'>
-                        </div>
-                        <div class='posted-by' style='color: #ACACAC;'>
-                            <a href='$added_by'>$first_name $last_name</a> $user_to &nbsp; &nbsp; &nbsp; &nbsp; $time_message
-                        </div>
-                        <div id='post-body'>
-                            $body
-                            <br>
-                        </div>
-                    </div>
-                    <hr>";
+        $page = $data["page"];
+        $userLoggedIn = $this->user_obj->getUsername();
+        if ($page == 1) {
+            $start = 0;
+        } else {
+            $start = ($page - 1) * $limit;
         }
 
+        $str = ""; // string to return
+        $data_query = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
+
+        if (myslqi_num_rows($data_query) > 0) {
+            $num_iterations = 0;
+            $count = 1;
+
+            while ($row = mysqli_fetch_array($data_query)) {
+                $id = $row["id"];
+                $body = $row["body"];
+                $added_by = $row["added_by"];
+                $date_time = $row["date_added"];
+
+                // Prepare user_to string so it can be included even if not posted to a user
+                if ($row["user_to"] == "none") {
+                    $user_to = "";
+                } else {
+                    $user_to_obj = new User($this->con, $row["user_to"]);
+                    $user_to_name = $user_to_object->getFirstAndLastName();
+                    $user_to = "to <a href='" . $row["user_to"] . "'>" . $user_to_name . "</a>";
+                }
+
+                // Check if user who posted, has their account closed
+                $added_by_obj = new User($this->con, $added_by);
+                if ($added_by_obj->isClosed()) {
+                    continue;
+                }
+
+                if ($num_iterations++ < $start) {
+                    continue;
+                }
+
+                // Once 10 posts have been loaded, break
+                if ($count > $limit) {
+                    break;
+                } else {
+                    $count++;
+                }
+
+                $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
+                $user_row = mysqli_fetch_array($user_details_query);
+                $first_name = $user_row["first_name"];
+                $last_name = $user_row["last_name"];
+                $profile_pic = $user_row["profile_pic"];
+
+                // Timeframe
+                $date_time_now = date("Y-m-d H:i:s");
+                $start_date = new DateTime($date_time); // Time of post
+                $end_date = new DateTime($date_time_now); // Current time
+                $interval = $start_date->diff($end_date); // Difference between dates
+                if ($interval->y >= 1) {
+                    if ($interval == 1) {
+                        $time_message = $interval->y . " year ago"; // 1 year ago
+                    } else {
+                        $time_message = $interval->y . " years ago"; // More then 1 year ago
+                    }
+                } elseif ($interval->m >= 1) {
+                    if ($interval->d == 0) {
+                        $days = " ago";
+                    } elseif ($interval->d == 1) {
+                        $days = $interval->d . " day ago";
+                    } else {
+                        $days = $interval->d . " days ago";
+                    }
+
+                    if ($interval->m == 1) {
+                        $time_message = $interval->m . " month" . $days;
+                    } else {
+                        $time_message = $interval->m . " months" . $days;
+                    }
+                } elseif ($interval->d >= 1) {
+                    if ($interval->d == 1) {
+                        $time_message = "Yesterday";
+                    } else {
+                        $time_message = $interval->d . " days ago";
+                    }
+                } elseif ($interval->h >= 1) {
+                    if ($interval->h == 1) {
+                        $time_message = $interval->h . " hour ago";
+                    } else {
+                        $time_message = $interval->h . " hours ago";
+                    }
+                } elseif ($interval->i >= 1) {
+                    if ($interval->i == 1) {
+                        $time_message = $interval->i . " minute ago";
+                    } else {
+                        $time_message = $interval->i . " minutes ago";
+                    }
+                } else {
+                    if ($interval->s < 30) {
+                        $time_message = "Just now";
+                    } else {
+                        $time_message = $interval->s . " seconds ago";
+                    }
+                }
+
+                $str .= "<div class='status-post'>
+                            <div class='post-profile-pic'>
+                                <img src='$profile_pic' width='50'>
+                            </div>
+                            <div class='posted-by' style='color: #ACACAC;'>
+                                <a href='$added_by'>$first_name $last_name</a> $user_to &nbsp; &nbsp; &nbsp; &nbsp; $time_message
+                            </div>
+                            <div id='post-body'>
+                                $body
+                                <br>
+                            </div>
+                        </div>
+                        <hr>";
+            }
+
+            if ($count > $limit) {
+                $str .= "<input type='hidden' class='next-page' value='" . ($page + 1) . "'>
+                        <input type='hidden' class='no-more-posts' value='false'>";
+            } else {
+                $str .= "<input type='hidden' class='no-more-posts' value='true'><p style='text-align: center;'>No more posts to show</p>";
+            }
+        }
         echo $str;
     }
 }
--- index.php
+++ index.php
@@ -27,12 +27,54 @@ if (isset($_POST["post"])) {
                 <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
                 <input type="submit" name="post" id="post_button" value="Post">
             </form>
-
-            <?php
-                $post = new Post($con, $userLoggedIn);
-                $post->loadPostsFriends();
-            ?>
+            <div class="posts-area"></div>
+            <img id="loading" src="assets/images/icons/loading.gif">
         </div>
+
+        <script>
+            var userLoggedIn = '<?php echo $userLoggedIn ?>';
+
+            $(document).ready(function() {
+                $('#loading').show();
+
+                $.ajax({
+                    url: "includes/handlers/ajax_load_posts.php",
+                    type: "POST",
+                    data: "page=1&userLoggedin=" + userLoggedIn,
+                    cache: false,
+                    success: function(data) {
+                        $('#loading').hide();
+                        $('.posts-area').html(data);
+                    }
+                });
+
+                $(window).scroll(function() {
+                    var height = $('.posts-area').height();
+                    var scroll_top = $(this).scrollTop();
+                    var page = $('.posts-area').find('.next-page').val();
+                    var no_more_posts = $('.posts-area').find('.no-more-posts').val();
+
+                    if((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && no_more_posts == 'false') {
+                        $('#loading').show();
+
+                        var ajax_reg = $.ajax({
+                            url: "includes/handlers/ajax_load_posts.php",
+                            type: "POST",
+                            data: "page=" + page + "&userLoggedin=" + userLoggedIn,
+                            cache: false,
+                            success: function(response) {
+                                $('posts-area').find('.next-page').remove();
+                                $('posts-area').find('.no-more-posts').remove();
+
+                                $('#loading').hide();
+                                $('.posts-area').append(response);
+                            }
+                        });
+                    }
+                    return false;
+                });
+            });
+        </script>
     </div>
 </body>
 </html>
